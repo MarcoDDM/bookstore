1d41fe7943598676d9efcdd518fe5dc8ef08df02
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-05-12 13:22:42 -0300

    Add props to Books component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Books = () => {
+const Books = ({ title, author }) => (
   <div>
-    <h3>Title</h3>
-    <p>Author</p>
-  </div>;
+    <h3>{title}</h3>
+    <p>{author}</p>
+  </div>
+);
+
+Books.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
 };
 
 export default Books;
