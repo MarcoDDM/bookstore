bb29036b3683c6577a3b8975e6faf570844f871c
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-05-12 13:26:38 -0300

    Change Link to NavLink on Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Navigation() {
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <NavLink to="/">Home</NavLink>
         </li>
         <li>
-          <Link to="/categories">Categories</Link>
+          <NavLink to="/categories">Categories</NavLink>
         </li>
       </ul>
     </nav>
