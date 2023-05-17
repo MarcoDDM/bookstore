81263cd3597b91f72fd2c0ecfa36eed9dc3820e5
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-05-17 18:14:31 -0300

    Remove duplicate files and fix folders

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addBook } from '../redux/booksSlice';
+import { addBook } from '../redux/books/booksSlice';
 
 const categories = [
   'Action',
